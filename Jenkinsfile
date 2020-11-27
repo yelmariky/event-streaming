36324fd9fbdcc998f7c7bb711acb3a50693690a1
lmsys <lmsys.contact@gmail.com>
2020-11-27 17:15:09 +0100

    add jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,10 +11,10 @@ pipeline {
       stage('Build') {
          steps {
             // Get some code from a GitHub repository
-            git 'https://github.com/yelmariky/event-streaming.git'
-
+            //git 'https://github.com/yelmariky/event-streaming.git'
+             echo 'hello build'
             // Run Maven on a Unix agent.
-            sh "mvn -Dmaven.test.failure.ignore=true clean package"
+            //sh "mvn -Dmaven.test.failure.ignore=true clean package"
 
             // To run Maven on a Windows agent, use
             // bat "mvn -Dmaven.test.failure.ignore=true clean package"
--- install/CI/instal-jenkins-with-helm.sh
+++ install/CI/instal-jenkins-with-helm.sh
@@ -6,4 +6,10 @@ helm install jenkins -n jenkins -f jenkins.values $chart
 ##echo Username: user
 echo Password: $(kubectl get secret --namespace jenkins jenkins -o jsonpath="{.data.jenkins-password}" | base64 --decode)
 
-#kubectl --namespace jenkins port-forward svc/jenkins 8080:8080
+#kubectl --namespace jenkins port-forward svc/jenkins 8080:80
+
+#instal sonarqube
+#helm repo add oteemocharts https://oteemo.github.io/charts
+helm install -f sonarqube-values.yaml sonarqube oteemocharts/sonarqube
+export POD_NAME=$(kubectl get pods --namespace default -l "app=sonarqube,release=sonarqube" -o jsonpath="{.items[0].metadata.name}")
+##kubectl port-forward $POD_NAME 9000:9000 -n default
