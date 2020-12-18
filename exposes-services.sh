df748f4343eea29898c0ce12a4303f5767311634
elmariky <yelmariky@gmail.com>
2020-12-18 16:00:32 +0100

    Update exposes-services.sh
    test webhook

--- exposes-services.sh
+++ exposes-services.sh
@@ -3,5 +3,5 @@
 kubectl port-forward svc/account-web 9987:80
 ### order-web
 kubectl port-forward svc/order-web 8899:80
-### payment-web
+### payment-web to webhook
 kubectl port-forward svc/payment-web 8886:80
